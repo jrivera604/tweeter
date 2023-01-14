f63b4a39d48a8cd81c2b824f5e4cc009e11390de
jrivera604 <joshrivera91@hotmail.com>
2023-01-14 21:09:57 +0000

    add dynamic tweets functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+$(function() {
+
+
+  const createTweetElement = function(tweet){
+
+    let $tweet =   $(`
+
+    <article class="tweet">
+    <header>
+    <img class="image" src="${tweet.user.avatars}"/>
+    <span class="name">${tweet.user.name}</span>
+    <span class="handle">${tweet.user.handle}</span>
+    </header>
+    <div>${tweet.content.text}</div>
+    <footer><span>${timeago.format(tweet.created_at)}</span><i class="fas fa-heart"></i><i class="fas fa-retweet"></i><i class="fas fa-flag"></i></footer>
+  </article>
+  `);
+    
+    return $tweet;
+
+  }
+
+
+  const renderTweets = function(tweets){
+
+    // loops through tweets
+    for (const tweet of tweets) {
+      let $tweet = createTweetElement(tweet);
+      $('.tweets-list').append($tweet);
+    }  
+  };
+
+  renderTweets(data);
+
+});
